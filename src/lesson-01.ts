99861ffe393adaed5b8722e27c12160aee0e2979
TKende <kendetompos@gmail.com>
2022-01-20 18:55:11 +0100

    error

--- src/lesson-01.ts
+++ src/lesson-01.ts
@@ -5,6 +5,7 @@ export default class calculator {
     // Hozd létre a következő változókat a megfelelő típusannotációval.
     
     /** @type {Array} points - számok tömbje, 1-től 10-ig egyesével. */
+    points: number[] = [1,2,3,4,5,6,7,8,9,10];
 
 
     /** 
@@ -13,7 +14,10 @@ export default class calculator {
      * Inicializáld a tömböt, legyen legalább két eleme.
      * @type {Array} users - minden elem egy felhasználó.
      */
-
+        users: Array<{name: string, email: string}> = [
+            {name: 'Kiss Jozsef', email: 'kissjozsi69'},
+            {name: 'Nagy Peter', email: 'pityuka77'}
+        ] ;
     
     /** 
      * A tömb minden eleme legyen egy objektum, amely egy terméket reprezentál.
@@ -22,12 +26,17 @@ export default class calculator {
      * Inicializáld a tömböt, legyen legalább két eleme.
      * @type {Array} products - minden elem egy termék.
      */    
-
+     products: Array<{name: string, price: number, stock: number}> = [
+        {name: 'sajt', price: 5, stock: 3},
+        {name: 'kolbasz', price: 2, stock: 1}
+     ];
     
     /** 
      * Inicializálj egy tuple típust, azaz adj neki értéket is.
      * @type {Tuple} role - number, string, boolean. 
      */
+    let role: [number,string,boolean];
+    role = [10,'sajt',true];
 
 
 }
